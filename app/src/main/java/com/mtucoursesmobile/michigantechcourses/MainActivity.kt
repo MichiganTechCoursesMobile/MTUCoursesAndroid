dd55f1dd4b02da1828b3629f3bf80361bab8a7cf
Luis Vervaet <lrvervae@mtu.edu>
2024-03-19 11:24:32 -0400

    Update indentation

--- app/src/main/java/com/mtucoursesmobile/michigantechcourses/MainActivity.kt
+++ app/src/main/java/com/mtucoursesmobile/michigantechcourses/MainActivity.kt
@@ -9,35 +9,39 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.mtucoursesmobile.michigantechcourses.ui.theme.MichiganTechCoursesTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MichiganTechCoursesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      MichiganTechCoursesTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+          Greeting("Android")
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+  Text(
+    text = "Hello $name!",
+    color = Color.Blue,
+    fontSize = 30.sp,
+    modifier = modifier
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MichiganTechCoursesTheme {
-        Greeting("Android")
-    }
+  MichiganTechCoursesTheme {
+    Greeting("Android")
+  }
 }
