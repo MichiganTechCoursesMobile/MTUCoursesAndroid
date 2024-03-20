e7ecbc464e04b2d8ea37240221f58514128556b6
Luis Vervaet <lrvervae@mtu.edu>
2024-03-20 11:52:59 -0400

    Reworked getting data (woo)

--- app/src/main/java/com/mtucoursesmobile/michigantechcourses/MainActivity.kt
+++ app/src/main/java/com/mtucoursesmobile/michigantechcourses/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -25,16 +26,22 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -42,167 +49,87 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.mtucoursesmobile.michigantechcourses.api.MTUCourses
+import com.mtucoursesmobile.michigantechcourses.api.getCourses
 import com.mtucoursesmobile.michigantechcourses.ui.theme.MichiganTechCoursesTheme
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.net.URL
 
-
-data class MTUCourses(
-  val id: String, val year: Int, val semester: String, val subject: String, val crse: String,
-  val title: String,
-  val description: String, val updatedAt: String, val deletedAt: String, val prereqs: String,
-  val offered: Array<String>, val minCredits: Double, val maxCredits: Double
-)
-
-fun getJSON(): Array<MTUCourses> {
-  var data =
-    URL("https://api.michigantechcourses.com/courses?semester=FALL&year=2024").readText()
-  val gson = Gson()
-  val arrayTutorialType = object : TypeToken<Array<MTUCourses>>() {}.type
-
-  var tutorials: Array<MTUCourses> = gson.fromJson(
-    data,
-    arrayTutorialType
-  )
-  return tutorials
-}
-
 class MainActivity : ComponentActivity() {
-
-
   @OptIn(ExperimentalMaterial3Api::class)
   override fun onCreate(savedInstanceState: Bundle?) {
-    val policy = ThreadPolicy.Builder().permitAll().build()
-    StrictMode.setThreadPolicy(policy)
     super.onCreate(savedInstanceState)
     setContent {
       MichiganTechCoursesTheme {
-        val JSONData: Array<MTUCourses> = runBlocking { getJSON() }
-        Log.d(
-          "hi",
-          JSONData.size.toString()
-        )
-        Scaffold(
-          bottomBar = {
-            BottomAppBar(
-              containerColor = MaterialTheme.colorScheme.primaryContainer,
-              contentColor = MaterialTheme.colorScheme.primary,
-            ) {
-              Text(
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                text = "Bottom"
-              )
+        Surface(
+          // on below line we are specifying modifier and color for our app
+          modifier = Modifier.fillMaxSize(),
+        ) {
+          // on the below line we are specifying the theme as scaffold.
+          Scaffold(
+
+            // in scaffold we are specifying top bar.
+            topBar = {
+
+              // inside top bar we are specifying background color.
+              TopAppBar(
+
+                // along with that we are specifying title for our top bar.
+                title = {
+
+                  // in the top bar we are specifying tile as a text
+                  Text(
+
+                    // on below line we are specifying
+                    // text to display in top app bar.
+                    text = "JSON Parsing in Android",
+
+                    // on below line we are specifying
+                    // modifier to fill max width.
+                    modifier = Modifier.fillMaxWidth(),
+
+                    // on below line we are
+                    // specifying text alignment.
+                    textAlign = TextAlign.Center,
+
+                    // on below line we are
+                    // specifying color for our text.
+                    color = Color.White
+                  )
+                })
+            }) { innerPadding ->
+            val context = LocalContext.current
+
+            val courseList = remember {
+              mutableStateListOf<MTUCourses>()
             }
-          },
-          floatingActionButton = {
-            FloatingActionButton(onClick = { /*TODO*/ }) {
-              Icon(
-                Icons.Default.Add,
-                contentDescription = "Add"
-              )
-            }
-          }) { innerPadding ->
-          CourseList(
-            innerPadding,
-            JSONData
-          )
+            getCourses(
+              courseList,
+              context
+            )
+            // on below line we are display list view
+            // method to display our list view.
+            DisplayListView(
+              innerPadding,
+              courseList
+            )
+          }
         }
       }
     }
   }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CourseList(innerPadding: PaddingValues, JSONData: Array<MTUCourses>) {
-  LazyColumn(
-    modifier = Modifier
-      .padding(innerPadding)
-      .fillMaxSize(),
-    horizontalAlignment = Alignment.CenterHorizontally,
-  ) {
-    items(JSONData.size) { item ->
-      val sheetState = rememberModalBottomSheetState()
-      val scope = rememberCoroutineScope()
-      var showBottomSheet by remember {
-        mutableStateOf(false)
-      }
-      ElevatedCard(
-        elevation = CardDefaults.cardElevation(
-          defaultElevation = 4.dp
-        ),
-        modifier = Modifier
-          .fillMaxWidth()
-          .height(200.dp)
-          .padding(10.dp),
-        onClick = {
-          showBottomSheet = true
-        }
-      ) {
-        Text(
-          text = "${JSONData[item].subject}${JSONData[item].crse} - ${JSONData[item].title}",
-          modifier = Modifier
-            .padding(
-              horizontal = 10.dp
-            )
-            .paddingFromBaseline(
-              top = 30.dp,
-              bottom = 10.dp
-            ),
-          fontWeight = FontWeight.Bold,
-          fontSize = 20.sp,
-          textAlign = TextAlign.Center,
-        )
-        Text(
-          text = JSONData[item].description,
-          modifier = Modifier.padding(horizontal = 10.dp),
-          maxLines = 4,
-          overflow = TextOverflow.Ellipsis
-        )
-      }
-      if (showBottomSheet) {
-        ModalBottomSheet(
-          onDismissRequest = { showBottomSheet = false },
-          sheetState = sheetState,
-          modifier = Modifier.height(500.dp)
-        ) {
-          Text(
-            text = "${JSONData[item].subject}${JSONData[item].crse} - ${JSONData[item].title}",
-            modifier = Modifier
-              .padding(
-                horizontal = 10.dp
-              )
-              .paddingFromBaseline(
-                top = 30.dp,
-                bottom = 10.dp
-              ),
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            textAlign = TextAlign.Center,
-          )
-          Text(
-            text = JSONData[item].description,
-            modifier = Modifier.padding(horizontal = 10.dp),
-          )
-          Button(
-            onClick = {
-              scope.launch { sheetState.hide() }.invokeOnCompletion {
-                if (!sheetState.isVisible) {
-                  showBottomSheet = false
-                }
-              }
-            },
-            modifier = Modifier
-              .fillMaxWidth()
-              .padding(10.dp)
-          ) {
-            Text("Hide")
-          }
-
-        }
-      }
+fun DisplayListView(innerPadding: PaddingValues, courseList: SnapshotStateList<MTUCourses>) {
+  LazyColumn(modifier = Modifier.padding(innerPadding)) {
+    items(courseList.size) { item ->
+      Text(
+        courseList[item].toString(),
+        modifier = Modifier.padding(15.dp)
+      )
+      Divider()
     }
   }
 }
---
+++ app/src/main/java/com/mtucoursesmobile/michigantechcourses/api/GetCourses.kt
@@ -0,0 +1,62 @@
+package com.mtucoursesmobile.michigantechcourses.api
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
+
+data class MTUCourses(
+  val id: String, val year: Int, val semester: String, val subject: String, val crse: String,
+  val title: String,
+  val description: String, val updatedAt: String, val deletedAt: String, val prereqs: String,
+  val offered: Array<String>, val minCredits: Double, val maxCredits: Double
+)
+
+interface RetroFitAPI {
+  @GET("courses?semester=FALL&year=2024")
+  fun getCourseData(): Call<ArrayList<MTUCourses>>
+}
+
+fun getCourses(courseList: MutableList<MTUCourses>, ctx: Context) {
+  val retrofit = Retrofit.Builder()
+    .baseUrl("https://api.michigantechcourses.com/")
+    .addConverterFactory(GsonConverterFactory.create()).build()
+  val retrofitAPI = retrofit.create(RetroFitAPI::class.java)
+
+  val call: Call<ArrayList<MTUCourses>> = retrofitAPI.getCourseData()
+
+  call!!.enqueue(object : Callback<ArrayList<MTUCourses>?> {
+    override fun onResponse(
+      call: Call<ArrayList<MTUCourses>?>,
+      response: Response<ArrayList<MTUCourses>?>
+    ) {
+      if (response.isSuccessful) {
+        var lst: ArrayList<MTUCourses> = ArrayList()
+
+        lst = response.body()!!
+
+        for (i in 0 until lst.size) {
+          courseList.add(lst[i])
+        }
+      }
+    }
+
+    override fun onFailure(call: Call<ArrayList<MTUCourses>?>, t: Throwable) {
+      Toast.makeText(
+        ctx,
+        "Failed to get data..",
+        Toast.LENGTH_SHORT
+      ).show()
+    }
+  })
+}
