446d34a0342f6c4eb090d5208515e48cf2c57467
Luis Vervaet <lrvervae@mtu.edu>
2024-03-23 21:30:00 -0400

    Cleanup

--- app/src/main/java/com/mtucoursesmobile/michigantechcourses/MainActivity.kt
+++ app/src/main/java/com/mtucoursesmobile/michigantechcourses/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
