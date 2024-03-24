3dca20d04b11da2d156c3447fa694c77c90ec873
Luis Vervaet <lrvervae@mtu.edu>
2024-03-23 21:06:25 -0400

    Now support Android 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
   defaultConfig {
     applicationId = "com.mtucoursesmobile.michigantechcourses"
-    minSdk = 31
+    minSdk = 29
     targetSdk = 34
     versionCode = 1
     versionName = "1.0"
