07c18d6ac928c68444636743c4e42d7a44cb34b7
Luis Vervaet <lrvervae@mtu.edu>
2024-10-23 12:39:57 -0400

    Version Bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "com.mtucoursesmobile.michigantechcourses"
     minSdk = 29
     targetSdk = 35
-    versionCode = 18
-    versionName = "v1.0.2"
+    versionCode = 19
+    versionName = "v1.0.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
